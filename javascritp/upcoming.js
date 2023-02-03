322b80b757abb634830e7b2cea009c2c5475be71
JuanBarberis <juanbarberis.97@gmail.com>
2023-02-03 11:52:59 -0300

    agregue file data a los js

---
+++ javascritp/upcoming.js
@@ -0,0 +1,22 @@
+
+
+
+const upCominEvent = document.querySelector(".tarjet")
+
+for(let item of data.events){
+  if (data.currentDate < item.date){
+    upCominEvent.innerHTML += `
+    <div class="card text-center m-3" style="width: 18rem;">
+                <img src=${item.image} class="card-img-top" alt="costume_party">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text">${item.category}</p>
+                        <div class="price-descption">
+                            <p class="card-text ms-2">Prices:${item.price}</p>
+                            <a href="./description.html" class="btn btn-outline-danger">Description</a>
+                        </div>
+                    </div>
+            </div>
+  `
+  }
+}
